824c8cfef1f49528d21bdf858dad2627eb0827ae
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-12-01 16:53:59 +0000

    big tidy up

--- haskell/bristermitten/day01/Main.hs
+++ haskell/bristermitten/day01/Main.hs
@@ -3,10 +3,12 @@
 module Main where
 import qualified Data.Char as Char
 import Data.Maybe (maybeToList)
+import Data.List (tails, find, isPrefixOf)
 
 getInput :: IO String
 getInput = readFile "input.txt"
 
+firstAndLast :: [b] -> (b, b)
 firstAndLast [] = error "No digits found"
 firstAndLast [x] = (x, x)
 firstAndLast xs = (head xs, last xs)
@@ -16,25 +18,21 @@ process = uncurry ((+) . (10 *)) . firstAndLast . map (read . pure) . filter Cha
 
 
 process2 :: String -> Int
-process2 = uncurry ((+) . (10 *)) . firstAndLast . runParseParts
+process2 = uncurry ((+) . (10 *)) . firstAndLast . concatMap runParseParts . tails
     where
         runParseParts :: String -> [Int]
         runParseParts [] = []
         runParseParts xs = let (n, rest) = parseParts xs in maybeToList n ++ runParseParts rest
 
+        prefixes = map (\s -> (show @Int s, s)) [0..9] ++
+            [
+            ("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
+
         parseParts :: String -> (Maybe Int, String)
         parseParts [] = (Nothing, [])
-        parseParts ('z':'e':'r':'o':xs) = (Just 0, 'e':'r':'o':xs)
-        parseParts ('o':'n':'e':xs) = (Just 1, 'n':'e':xs)
-        parseParts ('t':'w':'o':xs) = (Just 2, 'w':'o':xs)
-        parseParts ('t':'h':'r':'e':'e':xs) = (Just 3, 'h':'r':'e':'e':xs)
-        parseParts ('f':'o':'u':'r':xs) = (Just 4, 'o':'u':'r':xs)
-        parseParts ('f':'i':'v':'e':xs) = (Just 5, 'i':'v':'e':xs)
-        parseParts ('s':'i':'x':xs) = (Just 6, 'i':'x':xs)
-        parseParts ('s':'e':'v':'e':'n':xs) = (Just 7, 'e':'v':'e':'n':xs)
-        parseParts ('e':'i':'g':'h':'t':xs) = (Just 8, 'i':'g':'h':'t':xs)
-        parseParts ('n':'i':'n':'e':xs) = (Just 9, 'i':'n':'e':xs)
-        parseParts (x:xs) = if Char.isDigit x then (Just (read [x]), xs) else (Nothing, xs)
+        parseParts str = case find ((`isPrefixOf` str) . fst) prefixes of
+                    Just (p, n) -> (Just n, drop (length p) str)
+                    Nothing -> (Nothing, tail str)
 
 main :: IO ()
 main = do
